f433d0a96b44b36963e37236d92860a96f746a79
Krupali Patel <krupali.patel@green-apex.com>
2024-07-20 16:25:43 +0530

    checked empty string input and single number input in calucaltor

--- services/string_calculator.rb
+++ services/string_calculator.rb
@@ -3,4 +3,23 @@ class StringCalculator
 	def initialize(numbers)
 		@numbers = numbers
 	end
+  
+  #adds string numbers values
+  def self.add(numbers)
+		new(numbers).add
+	end
+
+  #addition of the numbers
+	def add
+		return 0 if numbers.empty?
+		return numbers.to_i if single_number? #check input is single number then just return it
+	end
+
+	private
+
+	attr_reader :numbers
+
+	def single_number?
+		numbers.length == 1
+	end
 end
