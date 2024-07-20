53be928164284ca199a659568d665dd66a17e716
Krupali Patel <krupali.patel@green-apex.com>
2024-07-20 17:21:34 +0530

    added test cases for all usecases

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -9,5 +9,29 @@ RSpec.describe StringCalculator, type: :service do
     it "returns the number itself for a single number in string" do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it "returns the sum of two numbers in string" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
+
+    it "returns the sum of numbers in string" do
+      expect(StringCalculator.add("1,2,3")).to eq(6)
+    end
+
+    it "checks new lines between numbers in string" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it "checks different delimiters in string" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it "raises an error for negative numbers in string" do
+      expect { StringCalculator.add("1,-2,3") }.to raise_error("negative numbers are not allowed: -2")
+    end
+
+    it "raises an error for multiple negative numbers in string" do
+      expect { StringCalculator.add("1,-2,-3") }.to raise_error("negative numbers are not allowed: -2, -3")
+    end
   end
 end
