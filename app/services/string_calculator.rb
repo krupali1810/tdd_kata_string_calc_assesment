f79ced69b314b15fdcec57125d5b7440580a2cb5
Krupali Patel <krupali.patel@green-apex.com>
2024-07-20 17:20:46 +0530

    worked on add functionality by covering all the usecases mentioned

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -13,6 +13,16 @@ class StringCalculator
 	def add
 		return 0 if numbers.empty?
 		return numbers.to_i if single_number? #check input is single number then just return it
+    
+    begin
+      number_list = extract_numbers #extract numbers from string input
+      puts "=====number_list======#{number_list}"
+      check_negatives(number_list)
+      number_list.sum
+    rescue => e
+      puts "Exception caught #{e.message}"
+      raise e 
+    end
 	end
 
 	private
@@ -21,5 +31,31 @@ class StringCalculator
 
 	def single_number?
 		numbers.length == 1
+	end
+
+  def custom_delimiter?
+		numbers.start_with?("//")
+	end
+
+	def delimiter
+		return /,|\n/ unless custom_delimiter?
+
+		custom_delimiter = numbers.match(%r{//(.)\n})[1]
+		Regexp.escape(custom_delimiter)
+	end
+
+	def extract_numbers
+		if custom_delimiter?
+			_, number_string = numbers.split("\n", 2)
+		else
+			number_string = numbers
+		end
+		number_string.split(/#{delimiter}/).map(&:to_i)
+	end
+
+  #check is there any negative numbers are there or not
+	def check_negatives(number_list)
+		negatives = number_list.select { |n| n < 0 }
+		raise "negative numbers are not allowed: #{negatives.join(', ')}" unless negatives.empty?
 	end
 end
